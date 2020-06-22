c84d7114e5af5c953497dada2d5a40a52863de43
pranabsarkar <sarkarpranab66@gmail.com>
2020-06-22 21:14:34 +0530

    'v1'

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 # Importing all necessary libraries 
 import cv2 
 import os
+import numpy as np
+from os.path import isfile, join
 
-def photo(FILENAME,FOLDER_NAME):
+def video_photo(FILENAME,FOLDER_NAME):
     # Read the video from specified path 
     cam = cv2.VideoCapture(FILENAME)
     try: 
@@ -39,4 +41,35 @@ def photo(FILENAME,FOLDER_NAME):
     cam.release() 
     cv2.destroyAllWindows() 
 
+"""
+pathIn = The path which contains all the images.
+pathOut= The output file name in .avi format.
+fps = Define the Frames Per Second.
 
+""" 
+
+def convert_frames_to_video(pathIn,pathOut,fps):
+    pathIn='./'+pathIn
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+ 
+    #for sorting the file names properly
+    files.sort(key = lambda x: int(x[5:-4]))
+ 
+    for i in range(len(files)):
+        filename=pathIn + files[i]
+        #reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        print(filename)
+        #inserting the frames into an image array
+        frame_array.append(img)
+ 
+    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+ 
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+ 
